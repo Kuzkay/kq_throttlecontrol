32f5d56eec40c4b6db45beb697bbd5c5f191875d
Matthew <Matthww@users.noreply.github.com>
2024-01-22 22:32:35 +0100

    Semi rewrite for optimization purposes

--- kq_throttlecontrol/client/client.lua
+++ kq_throttlecontrol/client/client.lua
@@ -1,20 +1,15 @@
 local maxRpm = Config.maxRpm
 local maxSpeed = Config.maxSpeed
+local enabled = false
 
-Citizen.CreateThread(function()
-    while true do
-        local sleep = 2000
-        
-        local playerPed = PlayerPedId()
-        if IsPedInAnyVehicle(playerPed) then
-            local veh = GetVehiclePedIsIn(playerPed)
-            
-            -- Whether or not the ped is the driver
-            if GetPedInVehicleSeat(veh, -1) == playerPed then
-                sleep = 50
-                local input = Config.keybinds.slow.input
-
-                if IsControlPressed(0, input) and math.abs(GetVehicleThrottleOffset(veh)) > 0.3 and GetEntitySpeed(veh) * 3.6 <= maxSpeed then
+local function TriggerThrottleControl()
+    CreateThread(function()
+        while enabled do
+            local playerPed = PlayerPedId()
+            sleep = 50
+            if IsPedInAnyVehicle(playerPed) then
+                local veh = GetVehiclePedIsIn(playerPed)
+                if math.abs(GetVehicleThrottleOffset(veh)) > 0.3 and GetEntitySpeed(veh) * 3.6 <= maxSpeed then
                     sleep = 1
 
                     local rpm = GetVehicleCurrentRpm(veh)
@@ -24,8 +19,30 @@ Citizen.CreateThread(function()
                     end
                 end
             end
+            Wait(sleep)
         end
-        
-        Citizen.Wait(sleep)
+    end)
+end
+
+RegisterCommand('+throttlecontrol', function()
+    local playerPed = PlayerPedId()
+    if IsPedInAnyVehicle(playerPed) then
+        local veh = GetVehiclePedIsIn(playerPed)
+
+        -- Whether or not the ped is the driver
+        if GetPedInVehicleSeat(veh, -1) == playerPed then
+            -- enables the throttle control loop and calls the function
+            enabled = true
+            TriggerThrottleControl()
+            print('enabled')
+        end
     end
-end)
+end, false)
+
+RegisterCommand('-throttlecontrol', function()
+    -- disables the throttle control loop
+    print('disabled')
+    enabled = false
+end, false)
+
+RegisterKeyMapping('+throttlecontrol', 'Throttle Control', 'keyboard', Config.keybinds.slow.input)
