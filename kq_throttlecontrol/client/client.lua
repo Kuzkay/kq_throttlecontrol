92f12c609d870b75837c4582df8b31daec58c147
Kuzkay <31634013+Kuzkay@users.noreply.github.com>
2024-08-28 08:23:05 +0200

    refactor: Reworked client.lua

--- kq_throttlecontrol/client/client.lua
+++ kq_throttlecontrol/client/client.lua
@@ -4,41 +4,42 @@ local throttleControlEnabled = Config.defaultSmoothing  -- Set based on defaultS
 local allowedClasses = Config.allowedClasses
 
 local function TriggerThrottleControl()
+    if not throttleControlEnabled then
+        return
+    end
+    
     CreateThread(function()
-        while true do
-            if throttleControlEnabled then
-                local vehicle = GetVehiclePedIsIn(PlayerPedId(), false)
-                local sleepDuration = 50
-                if vehicle and DoesEntityExist(vehicle) then
-                    local vehicleClass = GetVehicleClass(vehicle)
-                    if allowedClasses[vehicleClass] then
-                        local vehicleSpeed = GetEntitySpeed(vehicle) * 3.6
-                        local throttleOffset = math.abs(GetVehicleThrottleOffset(vehicle))
-                        if throttleOffset > 0.3 and vehicleSpeed <= maxSpeed then
-                            sleepDuration = 1
-                            local currentRpm = GetVehicleCurrentRpm(vehicle)
-                            if currentRpm > maxRpm then
-                                SetVehicleCurrentRpm(vehicle, maxRpm)
-                            end
+        while throttleControlEnabled do
+            local vehicle = GetVehiclePedIsIn(PlayerPedId(), false)
+            local sleepDuration = 50
+            if vehicle and DoesEntityExist(vehicle) then
+                local vehicleClass = GetVehicleClass(vehicle)
+                if allowedClasses[vehicleClass] then
+                    local vehicleSpeed = GetEntitySpeed(vehicle) * 3.6
+                    local throttleOffset = math.abs(GetVehicleThrottleOffset(vehicle))
+                    if throttleOffset > 0.3 and vehicleSpeed <= maxSpeed then
+                        sleepDuration = 1
+                        local currentRpm = GetVehicleCurrentRpm(vehicle)
+                        if currentRpm > maxRpm then
+                            SetVehicleCurrentRpm(vehicle, maxRpm)
                         end
                     end
                 end
-                Wait(sleepDuration)
-            else
-                Wait(100)
             end
+            Wait(sleepDuration)
         end
     end)
 end
 
 RegisterCommand('+throttlecontrol', function()
     throttleControlEnabled = not Config.defaultSmoothing
+    TriggerThrottleControl()
 end, false)
 
 RegisterCommand('-throttlecontrol', function()
     throttleControlEnabled = Config.defaultSmoothing
+    TriggerThrottleControl()
 end, false)
 
-TriggerThrottleControl()
 
 RegisterKeyMapping('+throttlecontrol', 'Disable/Enable throttle control while holding depending on config', 'keyboard', Config.keybinds.slow.input)
